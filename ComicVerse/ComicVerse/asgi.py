f272f49339a0528a681fb9ae70b17f2f649d58ba
samlexgif <favorocheido@gmail.com>
2026-09-15 02:03:49 +0100

    Update asgi.py

--- ComicVerse/ComicVerse/asgi.py
+++ ComicVerse/ComicVerse/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ComicVerse.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ComicVerse.ComicVerse.settings')
 
 application = get_asgi_application()
