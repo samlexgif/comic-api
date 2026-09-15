32b842ae05697b8a6c888e49471d6a263f751a54
samlexgif <favorocheido@gmail.com>
2026-09-15 02:03:07 +0100

    Update wsgi.py

--- ComicVerse/ComicVerse/wsgi.py
+++ ComicVerse/ComicVerse/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ComicVerse.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ComicVerse.ComicVerse.settings')
 
 application = get_wsgi_application()
