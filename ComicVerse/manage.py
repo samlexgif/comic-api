d5966eb1200c60a6a18ae7850d7149aa0235aa6e
samlexgif <favorocheido@gmail.com>
2026-09-15 02:04:26 +0100

    Update manage.py

--- ComicVerse/manage.py
+++ ComicVerse/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ComicVerse.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ComicVerse.ComicVerse.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
